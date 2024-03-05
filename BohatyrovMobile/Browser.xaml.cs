045c3e078ae31e1138378dcca498021a29ffef48
Oleksandr Bohatyrov <alexboggg361@gmail.com>
2024-03-05 11:21:17 +0200

    final version (maybe))

--- BohatyrovMobile/Browser.xaml.cs
+++ BohatyrovMobile/Browser.xaml.cs
@@ -14,17 +14,18 @@ namespace BohatyrovMobile
     public partial class Browser : ContentPage
     {
         private bool isPortrait;
-        Button tagasi, home;
+        Button tagasi, home, forward, favourites, historybtn;
         Grid gridTop;
         Entry addressEntry;
         //Entry tel_nr_email, text;
-        Picker picker;
+        Picker picker, pickerHistory;
         WebView webView;
         StackLayout st;
-        string[] lehed = new string[4] { "https://moodle.edu.ee/", "https://www.tthk.ee/", "https://github.com/", "https://www.w3schools.com/" };
-        string[] nimetused = new string[4] { "Moodle", "TTHK", "GitHub", "W3Schools" };
- 
+        List<string> lehed = new List<string> { "https://moodle.edu.ee/", "https://www.tthk.ee/", "https://github.com/", "https://www.w3schools.com/" };
+        List<string> nimetused = new List<string> { "Moodle", "TTHK", "GitHub", "W3Schools" };
+        List<string> history = new List<string>();
 
+        string result = "";
 
 
         public Browser()
@@ -33,10 +34,15 @@ namespace BohatyrovMobile
             picker = new Picker
             {
                 Title = "Browser",
-               
-              
+
+
             };
             isPortrait = true;
+
+            pickerHistory = new Picker
+            {
+
+            };
 
 
 
@@ -57,36 +63,64 @@ namespace BohatyrovMobile
             gridTop = new Grid
             {
                 HorizontalOptions = LayoutOptions.FillAndExpand,
-                RowDefinitions = { new RowDefinition {  } },
+                RowDefinitions = { new RowDefinition { } },
                 ColumnDefinitions = {
                 new ColumnDefinition { Width = GridLength.Star },
-                new ColumnDefinition { Width = GridLength.Auto },
-                new ColumnDefinition { Width = GridLength.Auto }
+                new ColumnDefinition { Width = GridLength.Star },
+                new ColumnDefinition { Width = GridLength.Star },
+                new ColumnDefinition { Width = GridLength.Star },
+                new ColumnDefinition { Width = GridLength.Star }
                 }
 
             };
 
-            tagasi = new Button 
-            { 
-               
+            tagasi = new Button
+            {
+
                 Text = "<",
-            
+
             };
             tagasi.Clicked += Tagasi_Clicked;
 
 
             home = new Button
             {
-                Text= "|",
+                Text = "|",
             };
             home.Clicked += Home_Clicked;
+
+            forward = new Button
+            {
+
+                Text = ">",
+
+            };
+            forward.Clicked += Forward_Clicked;
+
+
+            favourites = new Button
+            {
+
+                Text = "*",
+
+            };
+            favourites.Clicked += Favourites_Clicked;
+
+            historybtn = new Button
+            {
+
+                Text = "?",
+
+            };
+            historybtn.Clicked += Historybtn_Clicked; ;
 
 
             addressEntry = new Entry
             {
                 Placeholder = "Enter URL",
-                ReturnType = ReturnType.Go, 
+                ReturnType = ReturnType.Go,
                 HorizontalOptions = LayoutOptions.FillAndExpand,
+
 
             };
             addressEntry.Completed += AddressEntry_Completed; ;
@@ -103,22 +137,130 @@ namespace BohatyrovMobile
             };
 
 
+            SwipeGestureRecognizer swipe_right = new SwipeGestureRecognizer { Direction = SwipeDirection.Right };
+            swipe_right.Swiped += Swipe_Swiped1;
+            SwipeGestureRecognizer swipe_left = new SwipeGestureRecognizer { Direction = SwipeDirection.Left };
+            swipe_left.Swiped += Swipe_Swiped1;
+            SwipeGestureRecognizer swipe_up = new SwipeGestureRecognizer { Direction = SwipeDirection.Up };
+            swipe_up.Swiped += Swipe_Swiped1;
+ 
+
             gridTop.Children.Add(addressEntry, 0, 0);
-            gridTop.Children.Add(tagasi, 1, 0);
-            gridTop.Children.Add(home, 2, 0);
+            Grid.SetColumnSpan(addressEntry, 6);
+            gridTop.Children.Add(tagasi, 0, 1);
+            gridTop.Children.Add(home, 1, 1);
+            gridTop.Children.Add(forward, 2, 1);
+            gridTop.Children.Add(favourites, 3, 1);
+            gridTop.Children.Add(historybtn, 4, 1);
+
+            addressEntry.HorizontalOptions = LayoutOptions.FillAndExpand;
 
 
 
             picker.SelectedIndexChanged += Valime_leht_avamiseks;
             st = new StackLayout
             {
-                Children = {gridTop, picker, webView,/* tel_nr_email, sms_btn, call_btn, mail_btn*/ }
+                Children = { gridTop, picker, webView,/* tel_nr_email, sms_btn, call_btn, mail_btn*/ }
             };
+            st.GestureRecognizers.Add(swipe_right);
+            st.GestureRecognizers.Add(swipe_left);
+     
+            st.GestureRecognizers.Add(swipe_up);
 
             Content = st;
 
 
-       
+
+        }
+
+        private void Swipe_Swiped1(object sender, SwipedEventArgs e)
+        {
+         
+            switch (e.Direction)
+            {
+                case SwipeDirection.Right:
+                
+
+                    if (webView.CanGoBack)
+                    {
+
+                        webView.GoBack();
+                        UpdatePickerSelection();
+
+
+                    }
+                    break;
+                case SwipeDirection.Left:
+                    if (webView.CanGoForward)
+                    {
+                        webView.GoForward();
+                        UpdatePickerSelection();
+
+                    }
+
+                    break;
+                case SwipeDirection.Up:
+                    webView.Source = new UrlWebViewSource { Url = lehed[0] };
+                    break;
+              
+                default: break;
+            }
+        }
+        private void UpdatePickerSelection()
+        {
+            var currentUrl = webView.Source?.ToString();
+            var index = lehed.IndexOf(currentUrl);
+            if (index >= 0 && index < picker.Items.Count)
+            {
+                picker.SelectedIndex = index;
+            }
+        }
+
+        private async void Historybtn_Clicked(object sender, EventArgs e)
+        {
+            if (history.Count > 0)
+            {
+                string selectedUrl = await DisplayActionSheet("Recent Pages", "Cancel", null, history.ToArray());
+
+                if (selectedUrl != "Cancel")
+                {
+                    webView.Source = new UrlWebViewSource { Url = selectedUrl };
+                }
+            }
+            else
+            {
+                await DisplayAlert("History", "No recent pages", "OK");
+            }
+        }
+
+        private async void Favourites_Clicked(object sender, EventArgs e)
+        {
+            if (webView.Source is UrlWebViewSource urlWebViewSource)
+            {
+                string currentUrl = urlWebViewSource.Url;
+
+                lehed.Add(currentUrl);
+
+                result = await DisplayPromptAsync("Vali uus nimi", "Uus nimi");
+                nimetused.Add(result);
+                picker.Items.Add(result);
+                picker.SelectedIndex = nimetused.IndexOf(result);
+            }
+            else
+            {
+                await DisplayAlert("Error", "The current source is not a URL", "OK");
+            }
+        }
+
+
+
+        private void Forward_Clicked(object sender, EventArgs e)
+        {
+            if (webView.CanGoForward)
+            {
+                webView.GoForward();
+
+            }
         }
 
         private void Home_Clicked(object sender, EventArgs e)
@@ -130,7 +272,7 @@ namespace BohatyrovMobile
         {
             if (webView.CanGoBack)
             {
-                webView.GoBack(); 
+                webView.GoBack();
 
             }
         }
@@ -150,16 +292,41 @@ namespace BohatyrovMobile
 
             if (!string.IsNullOrWhiteSpace(url))
             {
-                webView.Source = new UrlWebViewSource { Url = "https://"+url };
-                
+                if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+                {
+                    url = "https://" + url;
+                }
+
+                webView.Source = new UrlWebViewSource { Url = url };
+                AddToHistory(url);
             }
         }
 
         private void Valime_leht_avamiseks(object sender, EventArgs e)
         {
-            webView.Source = new UrlWebViewSource {Url = lehed[picker.SelectedIndex] };
-        
+            if (picker.SelectedIndex >= 0 && picker.SelectedIndex < lehed.Count)
+            {
+                string selectedUrl = lehed[picker.SelectedIndex];
+                webView.Source = new UrlWebViewSource { Url = selectedUrl };
+                AddToHistory(selectedUrl);
+            }
         }
+        private void AddToHistory(string url)
+        {
+        
+            if (!history.Contains(url))
+            {
+                history.Add(url);
+            }
+         
+            if (history.Count > 5)
+            {
+                history.RemoveAt(0); 
+            }
+        }
+
+
+
 
         protected override void OnSizeAllocated(double width, double height)
         {
